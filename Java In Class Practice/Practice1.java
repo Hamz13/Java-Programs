1594a36a6cfd8fef23e1f4d3dcbe05e234defa27
Hamza Ali <141524342+Hamz13@users.noreply.github.com>
2023-08-17 20:27:57 -0500

    Update
    Update to file.

---
+++ Java In Class Practice/Practice1.java
@@ -0,0 +1,23 @@
+
+
+public class Practice1 
+{
+    public static void main(String[] args)
+    {
+    
+
+ // Declare a string variable named 'name' and assign a name to it
+ String School = "Seven Lakes Jr. High School";
+        int Phone=388333;
+ // Print the value of the 'name' variable
+ System.out.println(School+"\n"+Phone);
+
+ 
+  
+  String a="Sum of x,y and z:";
+int x=2;
+int y=1;
+int z=9;
+ System.out.println(a+x+y+z);
+    }
+}
